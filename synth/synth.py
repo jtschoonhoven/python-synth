aa8a81276b347f4fc3d1fefb0188762e48ff9d28
jtschoonhoven <jschoonhoven@lyft.com>
2017-11-27 16:17:52 -0800

    add decay stage to note lifecycle (fix pop)

--- synth/synth.py
+++ synth/synth.py
@@ -26,8 +26,10 @@ if TYPE_CHECKING:
 @attr.attrs(slots=True)
 class Note(object):
     midi_note = attr.attrib()                  # type: int
-    volume = attr.attrib(default=255)          # type: int
+    volume = attr.attrib(default=100)          # type: int
     velocity = attr.attrib(default=255)        # type: int
+    status = attr.attrib(default=0)            # type: int  # 0=off; 1=pressed; 2=decay;
+    is_active = attr.attrib(default=False)     # type: bool
     sample_generator = attr.attrib(init=None)  # type: Iterable[int]
 
 
@@ -72,6 +74,11 @@ class Instrument(object):
         # type: () -> Callable[Iterator]
         pass
 
+    @abstractmethod
+    def get_note(self, midi_note, volume=None, velocity=None):
+        # type: (int, Optional[int], Optional[int]) -> Note
+        pass
+
 
 @attr.attrs(slots=True)
 class Synth(Instrument):
@@ -90,16 +97,12 @@ class Synth(Instrument):
             self.sample_generator,
         )
 
+        # initialize all possible notes
+        for midi_note in range(256):
+            self._notes[midi_note] = self.get_note(midi_note)
+
     def note_on(self, note):
         # type: (Note) -> None
-        note.sample_generator = self.note_generator(
-            note.midi_note,
-            self.SAMPLES_PER_SECOND,
-        )
-
-        with self._notes_lock:
-            self._notes[note.midi_note] = note
-
         note_on_event = NoteEvent(event_type=NOTE_EVENTS['NOTE_ON'], note=note)
         self._notes_event_queue.append(note_on_event)
 
@@ -108,9 +111,52 @@ class Synth(Instrument):
         note_off_event = NoteEvent(event_type=NOTE_EVENTS['NOTE_OFF'], note=note)
         self._notes_event_queue.append(note_off_event)
 
+    def get_note(self, midi_note, volume=None, velocity=None):
+        # type: (int, Optional[int], Optional[int]) -> Note
+        note = self._notes.get(midi_note)
+
+        if note is None:
+            note = Note(midi_note)
+        if volume is not None:
+            note.volume = volume
+        if velocity is not None:
+            note.velocity = velocity
+
+        def sample_generator():
+            # type: (int, int) -> Iterable[int]
+            cycles_per_second = helpers.midi_note_to_frequency(midi_note)
+            samples_per_cycle = int(self.SAMPLES_PER_SECOND / cycles_per_second)
+
+            sample_amplitude_array = []  # type: List[int]
+
+            for sample_idx in range(samples_per_cycle):
+                # "normalize" the index of this sample as a float between 0 and 1
+                normalized_idx = sample_idx / samples_per_cycle
+                # calculate the amplitude for this frame as a float between -1 and 1
+                # NOTE: this could be approximated for better performance
+                relative_amplitide = math.cos(normalized_idx * 2 * math.pi) * -1
+                # scale the amplitude to an integer between 0 and 255 (inclusive)
+                scaled_amplitude = int(relative_amplitide * 127 + 128)
+                # add amplitude to byte array
+                sample_amplitude_array.append(scaled_amplitude)
+
+            while True:
+                if note.status == 0:
+                    yield 0
+                else:
+                    for sample_amplitude in sample_amplitude_array:
+                        yield sample_amplitude
+                    if note.status == 2:
+                        note.status = 0  # turn note off at end of cycle
+
+        note.sample_generator = sample_generator()
+
+        return note
+
     def sample_generator(self):
         # type: () -> Iterable[int]
         notes_on = set()  # type: Set[int]
+        dead_notes = set()  # type: Set[int]
 
         while True:
             num_amplitudes = 0
@@ -121,15 +167,25 @@ class Synth(Instrument):
                 note_event = self._notes_event_queue.popleft()
 
                 if note_event.event_type == NOTE_EVENTS['NOTE_ON']:
-                    notes_on.add(note_event.note.midi_note)
+                    note = self._notes[note_event.note.midi_note]
+                    note.status = 1
+                    notes_on.add(note.midi_note)
 
-                elif note_event.event_type == NOTE_EVENTS['NOTE_OFF']:
-                    notes_on.discard(note_event.note.midi_note)
+                if note_event.event_type == NOTE_EVENTS['NOTE_OFF']:
+                    note = self._notes[note_event.note.midi_note]
+                    note.status = 2
+
+            for midi_note in dead_notes:
+                notes_on.discard(midi_note)
+            dead_notes.clear()
 
             # then play each active note
             for midi_note in notes_on:
                 note = self._notes[midi_note]
                 sample_amplitude = next(note.sample_generator)
+
+                if note.status == 0:
+                    dead_notes.add(midi_note)
 
                 if note.volume:
                     sample_amplitude = sample_amplitude * (note.volume / 255)
@@ -138,33 +194,10 @@ class Synth(Instrument):
                 amplitudes_sum += sample_amplitude
 
             if num_amplitudes:
-                yield int(amplitudes_sum / num_amplitudes)
+                combined_samples = int(amplitudes_sum / num_amplitudes)
+                yield combined_samples
             else:
                 yield 0
-
-    @staticmethod
-    # @helpers.simple_cache
-    def note_generator(midi_note, samples_per_second):
-        # type: (int, int) -> Iterable[int]
-        cycles_per_second = helpers.midi_note_to_frequency(midi_note)
-        samples_per_cycle = int(samples_per_second / cycles_per_second)
-
-        sample_amplitude_array = []  # type: List[int]
-
-        for sample_idx in range(samples_per_cycle):
-            # "normalize" the index of this sample as a float between 0 and 1
-            normalized_idx = sample_idx / samples_per_cycle
-            # calculate the amplitude for this frame as a float between -1 and 1
-            # NOTE: this could be approximated for better performance
-            relative_amplitide = math.cos(normalized_idx * 2 * math.pi) * -1
-            # scale the amplitude to an integer between 0 and 255 (inclusive)
-            scaled_amplitude = int(relative_amplitide * 127 + 128)
-            # add amplitude to byte array
-            sample_amplitude_array.append(scaled_amplitude)
-
-        while True:
-            for sample_amplitude in sample_amplitude_array:
-                yield sample_amplitude
 
 
 def profile(num_runs):
